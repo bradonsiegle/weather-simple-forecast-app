67d21041ba33ad0c5c294bd0b53765920b77a540
Bradon Siegle <bradonsiegle@gmail.com>
2023-01-14 15:41:42 -0700

    update base to reflect new repository name

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: '/weather-simple/',
+	base: '/weather-simple-forecast-app/',
 	server: {
 		host: true,
 		port: '3000',
